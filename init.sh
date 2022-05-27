a5c0f2ab933977d0d641310097cb23fe065643de
Nikita <105357388+awesomepln@users.noreply.github.com>
2022-05-27 16:40:33 -0400

    Add files via upload
    Solves problem with vocabularylist.txt

---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir /usr/vocabulary
+cp vocabulary.sh /usr/vocabulary 
+cp vocabularylist.txt /usr/vocabulary 
--- vocabulary.sh
+++ vocabulary.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 username=$(whoami)
 echo "Hi, $username"
-
+#1
 read -p "Do you want to write the word (y/n)?" choice
 case "$choice" in 
   y|Y ) echo "Write your word" 
 	read word
 	echo "Write your translation"
 	read translation 
-	echo "$word - $translation" >> /*/vocabulary/vocabularylist.txt;;
+	echo "$word - $translation" >> /root/Desktop/github/vocabularylist.txt;;
   n|N ) echo "See you soon...";;
   * ) echo "invalid";;
 esac
 
+###
 addnew() {
 	echo "Add your new word"
 	read word
 	echo "Write your translation"
 	read translation 
-	echo "$word - $translation" >> /*/vocabulary/vocabularylist.txt
+	echo "$word - $translation" >> /root/Desktop/github/vocabulary/vocabularylist.txt
 	read -p "Want to add another one (y/n)?" choice
 case "$choice" in 
   y|Y ) addnew;;
